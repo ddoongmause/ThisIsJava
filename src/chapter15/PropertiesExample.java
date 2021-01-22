ba3f666097589051d1acd3e6e728ea6b34e34cf8
OWNER <OWNER@192.168.0.3>
2021-01-22 15:30:06 +0800

    P.750(PDF-197) 검색 기능을 강화시킨 컬렉션

---
+++ src/chapter15/PropertiesExample.java
@@ -0,0 +1,27 @@
+package chapter15;
+
+import java.io.FileReader;
+import java.net.URLDecoder;
+import java.util.Properties;
+
+public class PropertiesExample {
+
+	public static void main(String[] args) throws Exception {
+		Properties properties = new Properties();
+		String path = PropertiesExample.class.getResource("database.properties").getPath();
+		//경로에 한글이 있을 경우 한글을 복원
+		path = URLDecoder.decode(path, "utf-8");
+		properties.load(new FileReader(path));
+		
+		String driver = properties.getProperty("driver");
+		String url = properties.getProperty("url");
+		String username = properties.getProperty("username");
+		String password =properties.getProperty("password");
+		
+		System.out.println("driver : " + driver);
+		System.out.println("url : " + url);
+		System.out.println("username : " + username);
+		System.out.println("password : " + password);
+	}
+
+}
